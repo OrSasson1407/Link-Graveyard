0268a671d437ed22e631d613f29c6717a1d21c82
OrSasson1407 <orsasson1407@gmail.com>
2026-06-16 15:18:01 +0300

    finish Docker backend

--- backend-node/src/main.ts
+++ backend-node/src/main.ts
@@ -1,43 +1,9 @@
 ﻿import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { HttpExceptionFilter } from './common/filters/http-exception.filter';
-import { LoggerService } from './common/logger/logger.service';
-import * as Sentry from '@sentry/node';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { bufferLogs: true });
-
-  const logger = app.get(LoggerService);
-  app.useLogger(logger);
-
-  Sentry.init({ dsn: process.env.SENTRY_DSN });
-
-  app.setGlobalPrefix('api/v1');
-
-  app.enableCors({
-    origin: process.env.CORS_ORIGIN || '*',
-    credentials: true,
-  });
-
-  app.useGlobalPipes(
-    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true, transform: true }),
-  );
-
-  app.useGlobalFilters(new HttpExceptionFilter());
-
-  const config = new DocumentBuilder()
-    .setTitle('Link Graveyard API')
-    .setDescription('Backend API for Link Graveyard')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, document);
-
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  logger.log('Application running on port ' + port);
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+  console.log('🚀 API Gateway is awake and waiting for Claude!');
 }
 bootstrap();
